ffe67d239f37df85680860e2b7c3514f09db1eae
EricaMkleinkauf <162368774+EricaMkleinkauf@users.noreply.github.com>
2024-03-20 15:13:00 -0300

    Delete exercicofacul.js

--- exercicofacul.js
+++
@@ -1,9 +0,0 @@
-let maiorIdade = true 
-let ensinoMedioCompleto = true
-let outraFacul = true
-
-if (maiorIdade && ensinoMedioCompleto && outraFacul){
-    console.log("voce nao pode entrar")
-}else {
-    console.log ("parabens, voce pode entrar")
-}
