8d80862820c38c86ca02f4a8ceef711618be0d57
EricaMkleinkauf <162368774+EricaMkleinkauf@users.noreply.github.com>
2024-04-30 14:37:58 -0300

    Add files via upload

--- trabalho leo/dragao.js
+++ trabalho leo/dragao.js
@@ -22,16 +22,16 @@ const statusDragons = [
 
 
 
-const readline = require('readline-sync');
+const readline = require('readline-sync'); //instalar o readline no terminal
 
 function dragoes(){ //mostra se o array está vazio
     if (dragoes.length === 0)
     console.log('Este array esta vazio')
 }
 
-dragoes()
+dragoes()//chamando a funçao
 
-function exibirMenu() {
+function exibirMenu() { // funcao do menu de opçoes, que ao chamarmos vai aparecer tudin
     console.log('menu de opções ');
     console.log('1. adicionar ');
     console.log('2. atualizar o status ');
@@ -42,38 +42,38 @@ function exibirMenu() {
 
 }
 
-function adicionarPersonagem() {
-    let personagem = readline.question("Digite o nome do novo personagem: ");
-    let casa = readline.question("Digite a casa do personagem: ");
-    let statusdoPersonagem = readline.question("Digite o status do personagem (vivo, morto ou desconhecido): ")
-    dragao.push({ nome: personagem, casa: casa, status: statusdoPersonagem  });
+function adicionarPersonagem() { //funcao para adicionar um personagem
+    let personagem = readline.question("Digite o nome do novo personagem: "); // criar uma variavel personagem 
+    let casa = readline.question("Digite a casa do personagem: "); // criar uma variavel para a casa 
+    let statusdoPersonagem = readline.question("Digite o status do personagem (vivo, morto ou desconhecido): ") //criar uma variavel para o status do personagem
+    dragao.push({ nome: personagem, casa: casa, status: statusdoPersonagem  }); // dragao = nome do array vazio, push = adicionar os elementos no array
 
 }
 
-function novoStatus(){
-    const num = Number(readline.question("Digite o indice do personagem que você quer mudar o status:"))
-    const novoStatus = readline.question("Novo status do personagem (vivo/morto/desconhecido):")
+function novoStatus(){ // funcao para mudar o status do personagem
+    const num = Number(readline.question("Digite o indice do personagem que você quer mudar o status:"))// criamos a nova variavel, Number porque indice funciona a traves de numeros
+    const novoStatus = readline.question("Novo status do personagem (vivo/morto/desconhecido):") //criamos a variavel do novo status
 
-    dragao[num].status = novoStatus
+    dragao[num].status = novoStatus //dragao = nome do array, [num] = para o usuario decidir qual indice ela vai alterar, = novoStatus = passar para o status novo
     console.log('Agora seu status é', novoStatus)
 }
 
 
 
  
-function verificaDragões(){
-    let nomeCasa = readline.question("Digite o nome da casa:")
+function verificaDragões(){ //funao de verificar quantos dragoes tem em cada casa
+    let nomeCasa = readline.question("Digite o nome da casa:") //variavel para perguntar o nome das casas
   
-    if (nomeCasa == 'targaryen') {
+    if (nomeCasa == 'targaryen') {// se o nome da casa for igual a targaryen, o console.log vai mostrar a quantidade de dragoes na casa targaryen
     console.log("7 dragões")
-    } else if (nomeCasa == 'velaryon') {
+    } else if (nomeCasa == 'velaryon') {// se não, se o nome dacasa for igual a velaryon, o console vai mostrar a quantidade de dragoes na casa velaryon
         console.log ("5 dragões")
-    }else{console.log("0 dragões")}
+    }else{console.log("0 dragões")} // se nao for nenhuma dessas casas, mostra zero dragoes
     
 }
 
-function statusDragoes (){   
-for (let i = 0; i < statusDragons.length; i++) {
+function statusDragoes (){   //funcao para mostrar o status dos dragoes das casas
+for (let i = 0; i < statusDragons.length; i++) { //for = um comando que faz um trecho do programa se repetir, uma variavel igual a zero, lenght le o array
     
     console.log(`O dragão ${statusDragons[i].nome} está ${statusDragons[i].status}.`)
 }
